845c16e0858aba3fb6e1b7ccf4498c11717bdb5b
Tedi Lesmana <tedi.lesmana0123@gmail.com>
2026-01-29 09:34:43 +0700

    fix: koneksi database

--- config/index.js
+++ config/index.js
@@ -5,14 +5,14 @@ const CONFIG = {
     process.env.MONGODB_URL_PROD ||
     process.env.MONGODB_URI,
   port: parseInt(process.env.PORT || "3000", 10),
-  mongodb_max_pool_size: parseInt(process.env.MONGODB_MAX_POOL_SIZE || "50", 10),
+  mongodb_max_pool_size: parseInt(process.env.MONGODB_MAX_POOL_SIZE || "20", 10),
   mongodb_min_pool_size: parseInt(process.env.MONGODB_MIN_POOL_SIZE || "0", 10),
   mongodb_socket_timeout_ms: parseInt(
-    process.env.MONGODB_SOCKET_TIMEOUT_MS || "45000",
+    process.env.MONGODB_SOCKET_TIMEOUT_MS || "20000",
     10
   ),
   mongodb_server_selection_timeout_ms: parseInt(
-    process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MS || "15000",
+    process.env.MONGODB_SERVER_SELECTION_TIMEOUT_MS || "10000",
     10
   ),
 };
